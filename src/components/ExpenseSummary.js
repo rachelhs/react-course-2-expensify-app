c4da7d8112054e9eb2d79b8ec95a02ba0276e584
rachelhs <rachelsmith400@hotmail.co.uk>
2020-06-11 11:47:54 +0100

    format numbers

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import getExpenseTotal from '../selectors/expense-total';
 import { connect } from 'react-redux';
+import numeral from 'numeral';
 
 const ExpenseSummary = (props) => {
     const expenseTotal = getExpenseTotal(props.expenses);
     const expenseCount = props.expenses.length;
+    const expenseWord = expenseCount === 1 ? 'expense': 'expenses';
+    const formattedExpenseTotal = numeral(expenseTotal/100).format('$0,000.00');
     return (
         <div>
-        <p>Viewing ${expenseCount} expenses totalling ${expenseTotal/100}</p>
+        <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpenseTotal}</h1>
         </div>
     );
 };
